a6f330c517344dd484ee46689411db93f53d03fc
danmoop <dandurnev1@gmail.com>
2020-12-23 20:15:30 -0800

    User link on main & project pages

--- src/variables/constants.ts
+++ src/variables/constants.ts
@@ -1,5 +1,5 @@
-// export const API_URL: string = 'https://www.gitpals.com/api';
-// export const WS_URL: string = 'https://www.gitpals.com/gitpals-messages';
+export const API_URL: string = 'https://www.gitpals.com/api';
+export const WS_URL: string = 'https://www.gitpals.com/gitpals-messages';
 
-export const API_URL: string = 'http://localhost:1337/api';
-export const WS_URL: string = 'http://localhost:1337/gitpals-messages';
+//export const API_URL: string = 'http://localhost:1337/api';
+//export const WS_URL: string = 'http://localhost:1337/gitpals-messages';
